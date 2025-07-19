53d63971be702b6a3d3c5196c4702cb8a64d9da5
DigitalBc498 <anarusconi3@gmail.com>
2025-07-18 21:18:18 -0300

    correccion en settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*)ofs@3zef3804t**6=a50&*!!c9&ko*on-$h5o8awk5c+n!6e')
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['centro-masajes-turnos.onrender.com', 'localhost', '127.0.0.1']
+
 
 # Aplicaciones
 INSTALLED_APPS = [
